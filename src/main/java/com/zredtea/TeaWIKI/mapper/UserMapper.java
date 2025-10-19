04f3f9fec32f9bc3e11249dd66ca669f8528df5f
ZRedTea <ZRedTea@outlook.com>
2025-10-19 20:45:07 +0800

    重构 UserMapper.java 为 QueryWrapper形式

--- src/main/java/com/zredtea/TeaWIKI/mapper/UserMapper.java
+++ src/main/java/com/zredtea/TeaWIKI/mapper/UserMapper.java
@@ -1,5 +1,7 @@
 package com.zredtea.TeaWIKI.mapper;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
 import com.zredtea.TeaWIKI.entity.User;
@@ -13,49 +15,92 @@ public interface UserMapper extends BaseMapper<User> {
     /**
     *** 根据用户名查找用户
      */
-    User SelectByUsername(@Param("username") String username);
+    default User SelectByUsername(String username) {
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("username",username);
+        return selectOne(queryWrapper);
+    }
 
     /**
     *** 根据用户编号查找用户
      */
-    User SelectByUserId(@Param("userid")  Integer userid);
+    default User SelectByUserId(Integer userid) {
+        return selectById(userid);
+    };
 
     /**
     *** 根据用户名检查用户是否存在
      */
-    Integer CheckByUsername(@Param("username") String username);
+    default Boolean CheckByUsername(String username) {
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("username",username);
+        return selectCount(queryWrapper) > 0;
+    };
 
     /**
     *** 根据用户名返回用户编号
      */
-    Integer SelectIdByUsername(@Param("username") String username);
+    default Integer SelectIdByUsername(String username) {
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("username",username);
+        User user = selectOne(queryWrapper);
+        return user != null ? user.getUserId() : null;
+    };
 
     /**
     *** 返回所有用户列表
      */
-    List<User> SelectAllUsers();
+    default List<User> SelectAllUsers() {
+        LambdaQueryWrapper<User> wrapper = new LambdaQueryWrapper<>();
+        wrapper.orderByDesc(User::getCreatedAt);
+        return selectList(wrapper);
+    };
 
     /**
     *** 根据学院返回所有用户列表
      */
-    List<User> SelectAllUsersByDepartment(@Param("department") String department);
+    default List<User> SelectAllUsersByDepartment(String department) {
+        LambdaQueryWrapper<User> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(User::getDepartment,department);
+        wrapper.orderByDesc(User::getCreatedAt);
+        return selectList(wrapper);
+    };
 
     /**
     *** 更新用户头像
      */
-    void updateUserAvatar(@Param("userid") Integer userid,
-                          @Param("avatar") String avatar);
+    default Boolean updateUserAvatar(Integer userid,
+                                     String avatar) {
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("user_id",userid);
+        User user = selectOne(queryWrapper);
+        user.setAvatar(avatar);
+        return updateById(user) > 0;
+    };
 
     /**
     *** 更新用户密码
      */
-    void updateUserPassword(@Param("userid") Integer userid,
-                            @Param("password") String password,
-                            @Param("salt") String salt);
+    default Boolean updateUserPassword(Integer userid,
+                                    String password, String salt) {
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("user_id",userid);
+        User user = selectOne(queryWrapper);
+        user.setPassword(password);
+        user.setSalt(salt);
+        return updateById(user) > 0;
+    };
+
 
     /**
     *** 更新用户昵称
      */
-    void updateUserNickname(@Param("userid") Integer userid,
-                            @Param("nickname") String nickname);
+    default Boolean updateUserNickname(Integer userid,
+                                       String nickname) {
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("user_id",userid);
+        User user = selectOne(queryWrapper);
+        user.setNickname(nickname);
+        return updateById(user) > 0;
+    };
 }
