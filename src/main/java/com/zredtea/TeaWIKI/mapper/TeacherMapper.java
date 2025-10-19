56b95a2756b4d77896cd741b780e6f8599d041e4
ZRedTea <ZRedTea@outlook.com>
2025-10-19 20:32:59 +0800

    创建 TeacherMapper.java

--- src/main/java/com/zredtea/TeaWIKI/mapper/TeacherMapper.java
+++ src/main/java/com/zredtea/TeaWIKI/mapper/TeacherMapper.java
@@ -1,10 +1,15 @@
 package com.zredtea.TeaWIKI.mapper;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.zredtea.TeaWIKI.entity.Teacher;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface TeacherMapper extends BaseMapper<Teacher> {
-
+    default Teacher SelectByTeacherName(String teacherName){
+        QueryWrapper<Teacher> queryWrapper = new QueryWrapper<>();
+        queryWrapper.like("teacher_name",teacherName);
+        return selectOne(queryWrapper);
+    }
 }
